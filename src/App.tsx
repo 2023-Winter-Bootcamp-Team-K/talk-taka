34775d504f2e7150c9f46c0212124f0f4a8a459a
egg-silver <v6984@naver.com>
2024-01-06 01:56:52 +0900

    styled-component 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import Modal from './components/modal/Modal';
 function App() {
   return (
     <>
       {/* <RouterProvider router={router} /> */}
+      <Modal />
     </>
   );
 }
