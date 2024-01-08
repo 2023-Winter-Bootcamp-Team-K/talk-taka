d5998ae68893892e4f9ae83744842a29d7780c3a
oil-ramp-cat <103806022+oil-lamp-cat@users.noreply.github.com>
2024-01-08 15:55:05 +0900

    LoginInput publishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import Modal from './components/modal/Modal';
+import LoginInput from './components/common/LoginInput';
+import { GlobalStyle } from './styles/GlobalStyle';
+
 function App() {
-  return <>{/* <RouterProvider router={router} /> */}</>;
+  return (
+    <>
+      {/* <RouterProvider router={router} /> */}
+      <GlobalStyle />
+      <LoginInput />
+    </>
+  );
 }
 
 export default App;
--- src/components/common/LoginInput.tsx
+++ src/components/common/LoginInput.tsx
@@ -1,13 +1,57 @@
+import { styled } from 'styled-components';
+
 export default function LoginInput() {
   return (
-    <div className="flex flex-col text-gray-800 font-sans">
-      <div className="text-left pb-5 pl-3 text-base font-black">아이디</div>
-      <div>
-        <input
-          placeholder="영문 + 숫자"
-          className="bg-gray-100 w-80 pr-5 pl-4 py-2.5 text-base text-black shadow-inner border-2 focus:outline-none focus:border-green-600 rounded-xl"
-        ></input>
-      </div>
-    </div>
+    <LayOut>
+      <InputType>아이디</InputType>
+      <InputLayer>
+        <IdPwInput placeholder="영문 + 숫자"></IdPwInput>
+      </InputLayer>
+    </LayOut>
   );
 }
+
+const LayOut = styled.div`
+  width: 29.55rem;
+  height: 6.96669rem;
+`;
+
+const InputType = styled.div`
+  width: 5.32956rem;
+  height: 1.425rem;
+  color: #2c2c2c;
+  text-align: center;
+  font-family: Noto Sans;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin-bottom: 1.74rem;
+`;
+
+const InputLayer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 29.55rem;
+  height: 3.8rem;
+  border-radius: 0.875rem;
+  background: #f0f0f0;
+  align-items: center;
+`;
+
+const IdPwInput = styled.input`
+  all: unset;
+  display: flex;
+  width: 27.26438rem;
+  height: 1.84725rem;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #f0f0f0;
+  color: #b2b2b2;
+  font-family: Noto Sans;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
