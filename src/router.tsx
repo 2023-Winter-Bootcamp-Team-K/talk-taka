26f54d99b37a9ec7b51f775d77bfcba0f89ec4de
jongmine <joychoe21@icloud.com>
2024-01-09 22:50:57 +0900

    MainPage 퍼블리싱

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,14 @@
-// import { createBrowserRouter } from 'react-router-dom';
 import LoginPage from './pages/LogInPage';
 import { createBrowserRouter } from 'react-router-dom';
+import MainPage from './pages/MainPage';
 const router = createBrowserRouter([
   {
     path: '/login',
     element: <LoginPage />,
+  },
+  {
+    path: '/main',
+    element: <MainPage />,
   },
 ]);
 
