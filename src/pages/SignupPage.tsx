b7808940d11f8909455b9474ea0158b87f4b5fd0
oil-ramp-cat <103806022+oil-lamp-cat@users.noreply.github.com>
2024-01-11 21:29:19 +0900

    회원가입 반응형 페이지 퍼블리싱

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -26,16 +26,22 @@ export default function SignupPage() {
             <DirectionRow>
               <LoginInput
                 width="19.0625rem"
+                widthP="11.569rem"
                 type="아이디"
                 placeholder="영문 + 숫자"
                 marginBottom="0.47rem"
+                marginBottomPTT="0.06rem"
               ></LoginInput>
               <Button
                 title="중복확인"
                 width="9.1875rem"
+                widthP="5.8125rem"
                 marginL="1.31rem"
+                marginLP="0.76rem"
                 marginT="1.4rem"
+                marginTP="1.5rem"
                 fontS="1.125rem"
+                fontSP="0.875rem"
                 borderR="0.875rem"
               ></Button>
             </DirectionRow>
@@ -49,12 +55,15 @@ export default function SignupPage() {
               typeI="password"
               placeholder="6자리 이상"
               marginBottom="1.31rem"
+              marginBottomP="2.09rem"
+              marginBottomPTT="0.06rem"
             ></LoginInput>
             <LoginInput
               type="비밀번호 확인"
               typeI="password"
               placeholder="6자리 이상"
               marginBottom="0.47rem"
+              marginBottomPTT="0.06rem"
             ></LoginInput>
             <Left>
               <IdCheck>
@@ -77,11 +86,15 @@ export default function SignupPage() {
               type="이름"
               placeholder="자녀분의 이름을 입력해주세요."
               marginBottom="2.25rem"
+              marginBottomP="2.2rem"
+              marginBottomPTT="0.06rem"
             ></LoginInput>
             <LoginInput
               type="생년월일"
-              placeholder="자녀분의 나이를 입력해주세요 EX.) 2000-01-01"
+              placeholder="EX.) 2017-01-01"
               marginBottom="2.25rem"
+              marginBottomP="1.7rem"
+              marginBottomPTT="0.03rem"
             ></LoginInput>
             <GenderLayout>
               <Gender>성별</Gender>
@@ -103,7 +116,8 @@ export default function SignupPage() {
             <Button
               title="확인"
               width="14.24731rem"
-              marginT="1.06rem"
+              marginT="1.5rem"
+              marginTP="0.6rem"
               onClick={goToLogin}
             ></Button>
             <SignUp onClick={goToLogin}>로그인 하러가기</SignUp>
@@ -115,29 +129,45 @@ export default function SignupPage() {
 }
 
 const BackGround = styled.div`
-  background-image: url('src/assets/img/Login_bg.png');
+  z-index: 1;
   margin: auto;
-  width: 100vw;
-  height: 100vh;
   background-repeat: no-repeat;
-  background-position-x: 50%;
-  background-position-y: 75%;
   display: flex;
   text-align: center;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  width: 100vw;
+  height: 100vh;
+
+  @media all and (min-width: 791px) {
+    background-image: url('src/assets/img/Login_bg.png');
+    background-position-x: 50%;
+    background-position-y: 75%;
+  }
+  @media all and (max-width: 790px) {
+    background-position: center;
+    background-image: url('src/assets/img/LoginPage_p_bg.png');
+  }
 `;
 
 const LoginLayout = styled.div`
+  position: absolute;
   background-color: white;
-  width: 34.75rem;
-  height: 45.125rem;
   border-radius: 0.9375rem;
   background: var(--, #fffbfb);
   box-shadow: 16.5px -10px 28.2px 0px rgba(0, 0, 0, 0.25);
   display: flex;
   flex-direction: column;
   align-items: center;
+  @media all and (min-width: 791px) {
+    width: 34.75rem;
+    height: 45.125rem;
+  }
+  @media all and (max-width: 790px) {
+    width: 21.4375rem;
+    height: 32.625rem;
+  }
 `;
 
 const DirectionRow = styled.div`
@@ -148,57 +178,83 @@ const DirectionRow = styled.div`
 
 const Login = styled.div`
   display: flex;
-  width: 11.6875rem;
-  margin-bottom: 3.44rem;
   flex-direction: column;
   justify-content: center;
   color: #000;
-
   text-align: center;
-  font-size: 2.75rem;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
 
   margin-top: 2.94rem;
-  margin-bottom: 3.44rem;
+
+  @media all and (min-width: 791px) {
+    width: 11.6875rem;
+    height: 3.1875rem;
+    font-size: 2.75rem;
+    margin-bottom: 3.44rem;
+  }
+  @media all and (max-width: 790px) {
+    width: 4.41794rem;
+    height: 1.4375rem;
+    font-size: 1.125rem;
+    margin-bottom: 2.5rem;
+  }
 `;
 
 const Left = styled.div`
   display: flex;
   justify-content: left;
   width: 100%;
-  padding-left: 2.87rem;
+  @media all and (min-width: 791px) {
+    padding-left: 2.87rem;
+  }
+  @media all and (max-width: 790px) {
+    padding-left: 2rem;
+  }
 `;
 
 const IdCheck = styled.div`
-  width: 17.95856rem;
-  margin-bottom: 0.76;
   display: flex;
+
+  @media all and (min-width: 791px) {
+    width: 17.95856rem;
+    height: 1.84725rem;
+    margin-bottom: 0.76;
+  }
+  @media all and (max-width: 790px) {
+    width: 17.95856rem;
+    height: 1.84725rem;
+    margin-bottom: 1rem;
+  }
 `;
 
 const CheckMark = styled.div`
-  width: 0.9375rem;
-  height: 0.9375rem;
-  margin-bottom: 0.86rem;
-
   /* text */
   display: flex;
-  width: 16.89606rem;
-  height: 1.84725rem;
   flex-direction: row;
   justify-content: left;
   color: #78aa77;
-  font-size: 1.125rem;
+
   font-style: normal;
   font-weight: 400;
   line-height: normal;
+
+  @media all and (min-width: 791px) {
+    width: 17.95856rem;
+    height: 0.9375rem;
+    font-size: 1.125rem;
+  }
+  @media all and (max-width: 790px) {
+    width: 17.95856rem;
+    height: 1.84725rem;
+    font-size: 0.875rem;
+  }
 `;
 
 const SignUp = styled.button`
   all: unset;
-  margin-top: 1.06rem;
-  margin-bottom: 2.5rem;
+
   display: flex;
   width: 13.9375rem;
   flex-direction: column;
@@ -206,10 +262,20 @@ const SignUp = styled.button`
   color: #b2b2b2;
 
   text-align: center;
-  font-size: 1.125rem;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
+
+  @media all and (min-width: 791px) {
+    margin-top: 0.5rem;
+    margin-bottom: 2.5rem;
+    font-size: 1.125rem;
+  }
+  @media all and (max-width: 790px) {
+    margin-top: 0.7rem;
+    font-size: 0.875rem;
+  }
+
   cursor: pointer;
   text-decoration-line: underline;
   &:active {
@@ -226,11 +292,18 @@ const GenderButtonLayout = styled.div`
 
 const GenderButton = styled.button<{ isSelected: boolean }>`
   all: unset;
-  width: 5.75rem;
-  height: 7.4375rem;
 
   border-radius: 3.6875rem;
   background: #fffbfb;
+
+  @media all and (min-width: 791px) {
+    width: 5.75rem;
+    height: 7.4375rem;
+  }
+  @media all and (max-width: 790px) {
+    width: 4.625rem;
+    height: 5.5rem;
+  }
 
   ${({ isSelected }) =>
     isSelected &&
@@ -260,10 +333,16 @@ const Gender = styled.div`
 
   //text
   color: #2c2c2c;
-  font-size: 1.25rem;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
+
+  @media all and (min-width: 791px) {
+    font-size: 1.25rem;
+  }
+  @media all and (max-width: 790px) {
+    font-size: 1rem;
+  }
 `;
 
 const GenderLayout = styled.div`
