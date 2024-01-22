b2ae10b936fb8917957a9a8664e9f71416f2179f
fallkim <fs728kimgc@naver.com>
2024-01-23 01:54:02 +0900

    fix : tts 목소리 및 감정 변경

--- backend/tts.py
+++ backend/tts.py
@@ -9,7 +9,7 @@ def text_to_speach(text):
     client_secret = os.getenv("CLOVA_TTS_SECRET")
     encText = urllib.parse.quote(text)
     # 음성으로 변환할 텍스트와 음색, 속도, 감정 등 변경
-    data = "speaker=nara&volume=0&speed=0&pitch=0&format=mp3&text=" + encText;
+    data = "speaker=vdain&volume=0&speed=0&emotion=1&pitch=0&format=mp3&text=" + encText;
     url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
     request = urllib.request.Request(url)
     request.add_header("X-NCP-APIGW-API-KEY-ID", client_id)
