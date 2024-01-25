fd474cdf13aa2222d5db9f26b5e91b6121d8f6d7
sunghyun0610 <101376904+sunghyun0610@users.noreply.github.com>
2024-01-25 15:44:07 +0900

    feat : prompt 수정

--- backend/backend/diary/utils.py
+++ backend/backend/diary/utils.py
@@ -10,7 +10,7 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 # gpt한테 요약 요청
 def generate_summary(content):
     content_str = "\n".join(content)
-    summary_request = ('You have to write a picture diary based on the conversation. It\'s going to be in your child\'s picture diary. Please write 180 characters or less. And you only speak in Korean')
+    summary_request = ('When the conversation is over, summarize the conversation into 180 characters in maximum and write the contents of the child\'s drawing diary in Korean. Write it using a literacy style, and do not include the last conversation in the diary.')
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -30,7 +30,7 @@ def generate_image(summary):
 
     response = client.images.generate(
         model="dall-e-3",
-        prompt=summary,
+        prompt=f'{summary}Please generate a cute and lovely illustration based on this summary, using seed number 3096745504. The illustration should be a fairytale illustration suitable for a child\'s picture diary, ages 4 to 6. If there are people in the drawing, please depict them with a light apricot-colored skin tone (#fdece2) and draw black or dark brown hair. Exclude any text and provide only the illustration.',
         size="1024x1024",
         quality="standard",
         n=1,
