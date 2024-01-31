01c23691301f31ef3e3d218eb4ea8a54a40e5d04
fallkim <fs728kimgc@naver.com>
2024-02-01 00:30:33 +0900

    fix : url수정

--- frontend/src/api/config.ts
+++ frontend/src/api/config.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-// const BASE_URL = 'http://localhost:8000/api/v1';
-const BASE_URL = 'https://talktaka.site';
-
 export const baseInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: 'https://talktaka.site/api',
 });
 
 baseInstance.interceptors.response.use(
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,15 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*'],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://talktaka.site',
-        // target: 'http://localhost:8000',
-        // changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  assetsInclude: ['**/*.jpg'],
 });
