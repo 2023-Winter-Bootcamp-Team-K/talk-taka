5708aaa4d2d5f515ae31fb4592e4b94b8eebb060
fallkim <fs728kimgc@naver.com>
2024-01-31 22:59:07 +0900

    fix : config 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.jpg'],
+  assetsInclude: ['**/*'],
   server: {
     proxy: {
       '/api': {
         target: 'https://talktaka.site',
         // target: 'http://localhost:8000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        // changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
