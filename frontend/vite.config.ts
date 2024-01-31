1a4d55b5f83118e497ac27e3cd83b585f40f18a0
fallkim <fs728kimgc@naver.com>
2024-01-31 17:28:08 +0900

    fix : frontned vite.confg 수정:

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://talktaka.site',
+        changeOrigin: true,
         // target: 'http://localhost:8000',
       },
     },
