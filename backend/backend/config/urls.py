6407a47db52c452ca5ad2a1f3be96dcd62639570
fallkim <fs728kimgc@naver.com>
2024-01-31 17:40:07 +0900

    fix : backend url 수정

--- backend/backend/config/urls.py
+++ backend/backend/config/urls.py
@@ -33,10 +33,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/v1/", include("users.urls")),
-    path('api/v1/main/', include("main.urls")),
-    path("api/v1/apps/", include("apps.urls")),
-    path("api/v1/diary/", include("diary.urls")),
+    path("", include("users.urls")),
+    path('main/', include("main.urls")),
+    path("apps/", include("apps.urls")),
+    path("diary/", include("diary.urls")),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
