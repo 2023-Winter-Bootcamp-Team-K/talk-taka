24ef03a68a446ae111af308f28092e466abf1032
yunjuwon <a77265192@gmail.com>
2024-02-01 22:17:33 +0900

    chore :settings 병경

--- backend/backend/config/settings.py
+++ backend/backend/config/settings.py
@@ -74,9 +74,9 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = (
-    # "http://localhost:5173",
-    # "http://127.0.0.1:5173",
-    # "http://localhost:8000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:8000",
     "http://talktaka.site"
 )
 CORS_ALLOW_ALL_ORIGINS = True
